7bbac8d414694ddfd874de936ba550cf29fc4476
Carlos Rodrigues <carlos.1994hrs@gmail.com>
2024-08-07 23:17:56 -0300

    ci: improve cicd

--- ci/main.tf
+++ ci/main.tf
@@ -35,6 +35,33 @@ resource "aws_key_pair" "deployer" {
   public_key = var.ssh_ec2_public_key
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "graphql_server_instance_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "network_interface" {
+  subnet_id = aws_subnet.graphql_server_instance_subnet.id
+  # private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 
 resource "aws_security_group" "sg_8080" {
   name = "terraform-learn-state-sg-8080"
@@ -59,10 +86,16 @@ resource "aws_instance" "graphql_server_instance" {
   count                  = 1
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg_8080.id]
+
+  network_interface {
+    network_interface_id = aws_network_interface.network_interface.id
+    device_index         = 0
+  }
   tags = {
     Name = "graphql_server_instance_${count.index}"
   }
 }
+
 
 output "instance_ids" {
   value = aws_instance.graphql_server_instance[*].id
