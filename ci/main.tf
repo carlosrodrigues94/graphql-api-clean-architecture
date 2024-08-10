722e6d7e41637ac2133b49b84daa92ffcbe429d3
Carlos Rodrigues <carlos.1994hrs@gmail.com>
2024-08-10 17:09:26 -0300

    ci: fix deploy workflow

--- ci/main.tf
+++ ci/main.tf
@@ -140,23 +140,6 @@ resource "aws_network_interface" "graphql_server_network_i" {
   security_groups = [aws_security_group.graphql_instance_sg.id]
 }
 
-# resource "aws_instance" "graphql_server_instance" {
-#   depends_on             = [aws_vpc.graphql_server_instance_vpc]
-#   key_name               = aws_key_pair.deployer.key_name
-#   ami                    = data.aws_ami.ubuntu.id
-#   count                  = 1
-#   instance_type          = "t2.micro"
-#   vpc_security_group_ids = [aws_security_group.graphql_instance_sg.id]
-#   subnet_id              = aws_subnet.graphql_server_instance_subnet.id
-
-#   tags = {
-#     Name = "graphql_server_instance_${count.index}"
-#   }
-# }
-
-
-
-
 # 8) Assign Elastic IP to ENI
 resource "aws_eip" "server_elastic_ip" {
   vpc                       = true
@@ -170,13 +153,13 @@ resource "aws_eip" "server_elastic_ip" {
 }
 
 
-
 # 10) Create Linux Server and Install/Enable Apache2
 resource "aws_instance" "graphql_instance" {
   ami               = "ami-0947d2ba12ee1ff75"
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
   key_name          = aws_key_pair.deployer.key_name
+
   # iam_instance_profile = "${aws_iam_instance_profile.EC2-S3_Profile.name}"
 
   network_interface {
@@ -184,14 +167,14 @@ resource "aws_instance" "graphql_instance" {
     network_interface_id = aws_network_interface.graphql_server_network_i.id
   }
 
-  # user_data = <<-EOF
-  #   #!/bin/bash
-  #   sudo yum update -y
-  #   sudo yum install -y httpd.x86_64
-  #   sudo systemctl start httpd.service
-  #   sudo systemctl enable httpd.service
-  #   sudo aws s3 sync s3://awsbucketbeta00/website /var/www/html 
-  # EOF
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+  EOF
+
 
   tags = {
     Name = "graphql_instance"
@@ -201,7 +184,8 @@ resource "aws_instance" "graphql_instance" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_ecr_repository" "repository" {
-  name = var.repository_name
+  name         = var.repository_name
+  force_delete = true
 }
 
 
